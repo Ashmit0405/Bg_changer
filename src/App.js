053e2f0a36ea757ff585427b7db526e14b60643d
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2024-06-25 18:03:52 +0530

    Background Changer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  const [color,usecolor]=useState("olive");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="w-full h-screen flex flex-wrap justify-center"
+      style={{backgroundColor:color}}>
+        <h1 className='text-align-center text-3xl text-yellow-800'>Background Changer</h1>
+        <button className='fixed top-20 p-4 rounded-xl border border-orange-700' style={{backgroundColor:"olive"}} onClick={()=>usecolor("olive")}>
+          Default Color: Olive
+        </button>
+        <div className='fixed bottom-20 left-1 right-1'>
+        <div className="flex flex-wrap justify-around shadow-lg bg-black rounded-xl shadow-xl">
+        <button
+        className="border border-white rounded-xl px-4 py-1 text-white" style={{backgroundColor:"red"}} onClick={()=>usecolor("red")}>Red</button> 
+        <button
+        className="border border-white rounded-xl px-4 py-1 text-white" style={{backgroundColor:"green"}} onClick={()=>usecolor("green")}>Green</button> 
+        <button
+        className="border border-white rounded-xl px-4 py-1 text-white" style={{backgroundColor:"blue"}} onClick={()=>usecolor("blue")}>Blue</button> 
+        <button
+        className="border border-white rounded-xl px-4 py-1 text-white" style={{backgroundColor:"orange"}} onClick={()=>usecolor("orange")}>Orange</button> 
+        <button
+        className="border border-white rounded-xl px-4 py-1 text-black" style={{backgroundColor:"aliceblue"}} onClick={()=>usecolor("aliceblue")}>Aliceblue</button> 
+        <button
+        className="border border-white rounded-xl px-4 py-1 text-black" style={{backgroundColor:"cyan"}} onClick={()=>usecolor("cyan")}>Cyan</button> 
+        <button
+        className="border border-white rounded-xl px-4 py-1 text-white" style={{backgroundColor:"gray"}} onClick={()=>usecolor("gray")}>Gray</button> 
+        <button
+        className="border border-white rounded-xl px-4 py-1 text-white" style={{backgroundColor:"black"}} onClick={()=>usecolor("black")}>Black</button> 
+        <button
+        className="border border-white rounded-xl px-4 py-1 text-black" style={{backgroundColor:"pink"}} onClick={()=>usecolor("pink")}>Pink</button> 
+        </div>
+        </div>
+      </div>
   );
 }
 
